b3ee94ace3d33c8a60ed0b7c18223f26e01382f6
hemil nakrani <nakranihemil123@gmail.com>
2024-07-30 10:07:39 +0530

    Update user.router.js

--- router/user.router.js
+++ router/user.router.js
@@ -5,15 +5,20 @@ var middleware = require('../middleware/user.middleware.js')
 
 router.get('/login',controller.loginPage)
 router.get('/home',middleware,controller.Home)
-// router.get('/syllabus',controller.syllabus)
-// router.get('/update/:id',controller.updatepage)
 router.get('/register',controller.registerPage)
+router.get('/shop',controller.shopPage)
+router.get('/blog',controller.blogPage)
+router.get('/about',controller.aboutPage)
+router.get('/contact',controller.contactPage)
+router.get('/cart',controller.cartPage)
 
 
 router.post('/register', controller.register)
-// router.post("/updateData/:id",controller.updateData)
 router.post('/login',controller.login)
 
+// router.post("/updateData/:id",controller.updateData)
+// router.get('/syllabus',controller.syllabus)
+// router.get('/update/:id',controller.updatepage)
 
 router.get('/logout',middleware,(req,res) =>{
     res.clearCookie("token")
